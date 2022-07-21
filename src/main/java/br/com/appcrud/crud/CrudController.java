7560c668d90c4f01816f9aa9e4f2087ead535976
AlissonFerreiraEvangelista <alisson.22559@gmail.com>
2022-07-21 15:15:06 -0300

    commit do delete e update

--- src/main/java/br/com/appcrud/crud/CrudController.java
+++ src/main/java/br/com/appcrud/crud/CrudController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -42,10 +44,16 @@ public class CrudController {
         return consulta;
 
     }
-    @GetMapping("/delete/{id}")
+    @DeleteMapping("/delete/{id}")
     public void getDeletar(@PathVariable Long id){
         crudeRepositorio.deleteById(id);
 
+    }
+
+    @PutMapping("/update")
+    public CrudEntidade updateCadastro(@RequestBody CrudEntidade updateConsulta) {
+        CrudEntidade update = crudeRepositorio.save(updateConsulta);
+        return update;
     }
    
     
